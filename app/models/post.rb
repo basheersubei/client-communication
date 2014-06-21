8d8f68b56be9c62dfccb0949762e992a4e213a67
basheersubei <basheersubei@basheersubei-ThinkPad-X200.(none)>
2014-06-21 17:14:55 -0500

    oops removed post validates user_id (it shouldnt even have a user_id, but Comment should)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
-  validates :user_id, presence:true
   validates :title, presence: true
   validates :content, presence: true
   
